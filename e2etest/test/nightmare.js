f76893b4b27a7b977e6ddcce2cea227345ed9833
Atsushi Takayama <taka.atsushi@gmail.com>
2016-03-07 00:43:46 +0900

    インデント調整とシンタックスエラー解消

--- e2etest/test/nightmare.js
+++ e2etest/test/nightmare.js
@@ -5,50 +5,50 @@ import {expect} from 'chai';
 const baseurl = process.env.TARGET_URL || "http://localhost:8080";
 
 const option = {
-    show: true,
-      waitTimeout: 10000,
+  show: true,
+  waitTimeout: 10000,
 };
 
 const shortWait = 100;
 
 describe('e2etest', () => {
-    let nightmare;
+  let nightmare;
 
-    before(async () => {
-      nightmare = new Nightmare(option);
-      await nightmare
-      .goto(`${baseurl}/logout`)
-      .goto(`${baseurl}/initialize`)
-      .goto(`${baseurl}/`)
+  before(async () => {
+    nightmare = new Nightmare(option);
+    await nightmare
+    .goto(`${baseurl}/logout`)
+    .goto(`${baseurl}/initialize`)
+    .goto(`${baseurl}/`)
+  });
+  after(async () => {
+    await nightmare.end();
+  });
+
+  beforeEach(async () => {
+    await nightmare.wait(shortWait);
+  });
+
+  it('should register', async () => {
+    await nightmare
+    .goto(`${baseurl}/register`)
+    .wait(shortWait)
+    .type('input[name=account_name]', 'catatsuy')
+    .type('input[name=password]', 'catatsuy')
+    .click('input[type=submit]')
+    .wait(shortWait)
+    .goto(`${baseurl}/logout`)
+    .wait(shortWait)
+    .goto(`${baseurl}/login`)
+    .type('input[name=account_name]', 'catatsuy')
+    .type('input[name=password]', 'catatsuy')
+    .click('input[type=submit]')
+    .wait(shortWait)
+    .goto(`${baseurl}/mypage`);
+
+    const name = await nightmare.evaluate(function() {
+      return document.querySelector('.isu-account-name a').textContent;
     });
-    after(async () => {
-      await nightmare.end();
-    });
-
-    beforeEach(async () => {
-      await nightmare.wait(shortWait);
-    });
-
-    it('should register', async () => {
-      await nightmare
-      .goto(`${baseurl}/register`)
-      .wait(shortWait)
-      .type('input[name=account_name]', 'catatsuy')
-      .type('input[name=password]', 'catatsuy')
-      .click('input[type=submit]')
-      .wait(shortWait)
-      .goto(`${baseurl}/logout`)
-      .wait(shortWait)
-      .goto(`${baseurl}/login`)
-      .type('input[name=account_name]', 'catatsuy')
-      .type('input[name=password]', 'catatsuy')
-      .click('input[type=submit]')
-      .wait(shortWait)
-      .goto(`${baseurl}/mypage`);
-
-      const name = await nightmare.evaluate(function() {
-        return document.querySelector('.isu-account-name a').textContent;
-      });
-      expect('catatsuyさん').to.equal(name);
-
+    expect('catatsuyさん').to.equal(name);
+  });
 });
