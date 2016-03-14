c288c77d7e248b450abdf65431d763ea70365318
Atsushi Takayama <taka.atsushi@gmail.com>
2016-03-15 04:06:20 +0900

    cli.goが巨大になりすぎてきたのでprepareUserdataをuserdata.goに移動

--- benchmarker/cli.go
+++ benchmarker/cli.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"net/url"
-	"os"
-	"path/filepath"
 	"regexp"
 	"strings"
 	"time"
@@ -217,75 +213,6 @@ func makeChanBool(len int) chan bool {
 		ch <- true
 	}
 	return ch
-}
-
-func prepareUserdata(userdata string) ([]user, []user, []user, []string, []*checker.Asset, error) {
-	if userdata == "" {
-		return nil, nil, nil, nil, nil, errors.New("userdataディレクトリが指定されていません")
-	}
-	info, err := os.Stat(userdata)
-	if err != nil {
-		return nil, nil, nil, nil, nil, err
-	}
-	if !info.IsDir() {
-		return nil, nil, nil, nil, nil, errors.New("userdataがディレクトリではありません")
-	}
-
-	file, err := os.Open(userdata + "/names.txt")
-	if err != nil {
-		return nil, nil, nil, nil, nil, err
-	}
-	defer file.Close()
-
-	users := []user{}
-	bannedUsers := []user{}
-
-	scanner := bufio.NewScanner(file)
-	i := 1
-	for scanner.Scan() {
-		name := scanner.Text()
-		if i%50 == 0 { // 50で割れる場合はbanされたユーザー
-			bannedUsers = append(users, user{AccountName: name, Password: name + name})
-		} else {
-			users = append(users, user{AccountName: name, Password: name + name})
-		}
-		i++
-	}
-	adminUsers := users[:10]
-
-	sentenceFile, err := os.Open(userdata + "/kaomoji.txt")
-	if err != nil {
-		return nil, nil, nil, nil, nil, err
-	}
-	defer sentenceFile.Close()
-
-	sentences := []string{}
-
-	sScanner := bufio.NewScanner(sentenceFile)
-	for sScanner.Scan() {
-		sentence := sScanner.Text()
-		sentences = append(sentences, sentence)
-	}
-
-	imgs, err := filepath.Glob(userdata + "/img/000*") // 00001.jpg, 00002.png, 00003.gif など
-	if err != nil {
-		return nil, nil, nil, nil, nil, err
-	}
-
-	images := []*checker.Asset{}
-
-	for _, img := range imgs {
-		data, err := ioutil.ReadFile(img)
-		if err != nil {
-			return nil, nil, nil, nil, nil, err
-		}
-		images = append(images, &checker.Asset{
-			MD5:  util.GetMD5(data),
-			Path: img,
-		})
-	}
-
-	return users, bannedUsers, adminUsers, sentences, images, err
 }
 
 func checkUserpageNotLogin(s *checker.Session, users []user) {
