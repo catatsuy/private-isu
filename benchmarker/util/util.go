5f1817b5218c6c32e968a53bc49c632577224a7b
catatsuy <catatsuy@catatsuy.org>
2016-03-19 01:39:14 +0900

    未使用関数削除

--- benchmarker/util/util.go
+++ benchmarker/util/util.go
@@ -2,8 +2,6 @@ package util
 
 import (
 	"crypto/md5"
-	crand "crypto/rand"
-	"encoding/base64"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -37,22 +35,10 @@ func RandomLUNStr(n int) string {
 	return randomStr(n, lunRunes)
 }
 
-func RandomLNStr(n int) string {
-	return randomStr(n, lnRunes)
-}
-
 func randomStr(n int, s []rune) string {
 	buf := make([]byte, 0, n)
 	for i := 0; i < n; i++ {
 		buf = append(buf, byte(s[random.Int()%len(s)]))
 	}
 	return string(buf)
-}
-
-func SecureRandomStr(n int) string {
-	k := make([]byte, n)
-	if _, err := io.ReadFull(crand.Reader, k); err != nil {
-		return ""
-	}
-	return base64.StdEncoding.EncodeToString(k)
 }
