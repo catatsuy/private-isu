59104938fc423da2b573fadabd3a73f7716c8c4d
cyrill <cyrill@pixiv.com>
2016-03-19 19:05:02 +0900

    Use puma insteadof thin

---
+++ portal/config/puma.rb
@@ -0,0 +1,13 @@
+threads 8,32
+workers 3
+preload_app!
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
+
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect!
+end
