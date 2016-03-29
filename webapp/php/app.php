22fb3f1e6b04c52d2bb93de5629813fe37b1f77b
cyrill <cyrill@pixiv.com>
2016-03-29 22:23:17 +0900

    Remove date format

--- webapp/php/app.php
+++ webapp/php/app.php
@@ -310,7 +310,7 @@ $app->get('/posts', function (Request $request, Response $response) {
     $max_created_at = $params['max_created_at'];
     $db = $this->get('db');
     $ps = $db->prepare('SELECT `id`, `user_id`, `body`, `created_at`, `mime` FROM `posts` WHERE `created_at` <= ? ORDER BY `created_at` DESC');
-    $ps->execute([$max_created_at === null ? null : date(DATE_ATOM, $max_created_at)]);
+    $ps->execute([$max_created_at === null ? null : $max_created_at]);
     $results = $ps->fetchAll(PDO::FETCH_ASSOC);
     $posts = make_posts($results);
 
