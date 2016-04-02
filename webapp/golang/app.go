4a3c4a516dac491c06e1446af8987c3d5daea19d
catatsuy <catatsuy@catatsuy.org>
2016-04-03 00:57:45 +0900

    ログイン成功した
    シェル叩くのがつらすぎ

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -5,8 +5,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"path"
 	"strconv"
+	"strings"
 	"text/template"
 	"time"
 
@@ -14,7 +16,6 @@ import (
 	"github.com/gorilla/sessions"
 	"github.com/jmoiron/sqlx"
 	"github.com/zenazn/goji"
-	"github.com/zenazn/goji/web"
 )
 
 var (
@@ -48,17 +49,84 @@ type comment struct {
 	CreatedAt time.Time `db:"created_at"`
 }
 
-func getSessionUser() user {
-	return user{}
+func init() {
+	store = sessions.NewCookieStore([]byte("Iscogram"))
+}
+
+func getSession(r *http.Request) *sessions.Session {
+	session, err := store.Get(r, "isuconp-go.session")
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	return session
+}
+
+func getSessionUser(r *http.Request) *user {
+	session := getSession(r)
+	uid, ok := session.Values["user_id"]
+	if !ok || uid == nil {
+		return nil
+	}
+
+	u := &user{}
+
+	err := db.Get(&u, "SELECT * FROM `users` WHERE `id` = ?", uid)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	return u
+}
+
+func tryLogin(accountName, password string) *user {
+	u := user{}
+	err := db.Get(&u, "SELECT * FROM users WHERE account_name = ? AND del_flg = 0", accountName)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	if &u != nil && calculatePasshash(u.AccountName, password) == u.Passhash {
+		return &u
+	} else if &u == nil {
+		return nil
+	} else {
+		return nil
+	}
+
+	return &u
+}
+
+func digest(src string) string {
+	fmt.Println(src)
+	// TODO: escape
+	out, err := exec.Command("/bin/bash", "-c", `printf "%s" "`+src+`" | openssl dgst -sha512 | sed 's/^.*= //'`).Output()
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	fmt.Println(strings.TrimSuffix(string(out), "\n"))
+	return strings.TrimSuffix(string(out), "\n")
+}
+
+func calculateSalt(accountName string) string {
+	return digest(accountName)
+}
+
+func calculatePasshash(accountName, password string) string {
+	return digest(password + ":" + calculateSalt(accountName))
 }
 
 func getTemplPath(filename string) string {
 	return path.Join("templates", filename)
 }
 
-func getIndex(c web.C, w http.ResponseWriter, r *http.Request) {
+func getIndex(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "Hello, %s!", c.URLParams["name"])
-	_ = getSessionUser()
+	_ = getSessionUser(r)
 
 	posts := []post{}
 
@@ -74,6 +142,28 @@ func getIndex(c web.C, w http.ResponseWriter, r *http.Request) {
 		getTemplPath("posts.html"),
 		getTemplPath("post.html")),
 	).Execute(w, struct{ Posts []post }{posts})
+}
+
+func postLogin(w http.ResponseWriter, r *http.Request) {
+	if getSessionUser(r) != nil {
+		w.Header().Set("Location", "/")
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+
+	u := tryLogin(r.FormValue("account_name"), r.FormValue("password"))
+
+	if u != nil {
+		session := getSession(r)
+		session.Values["user_id"] = u.ID
+		session.Save(r, w)
+
+		w.Header().Set("Location", "/")
+		w.WriteHeader(http.StatusFound)
+	} else {
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusFound)
+	}
 }
 
 func main() {
@@ -115,5 +205,6 @@ func main() {
 	defer db.Close()
 
 	goji.Get("/", getIndex)
+	goji.Post("/login", postLogin)
 	goji.Serve()
 }
