a6d7d0e81f3d8495a20cbe79273a2ed04eb8a8c0
Atsushi Takayama <taka.atsushi@gmail.com>
2016-04-09 15:54:01 +0900

    enqueued?メソッドをつくる

--- portal/app/models/job.rb
+++ portal/app/models/job.rb
@@ -1,6 +1,4 @@
 class Job < ActiveRecord::Base
-  DEFAULT_TIMEOUT = 60.seconds
-
   belongs_to :team
 
   def running?
@@ -11,12 +9,7 @@ class Job < ActiveRecord::Base
     status == 'Waiting'
   end
 
-  def self.time_wait?(team:)
-    where(team: team).where('created_at > ?', Time.current - timeout).count > 0
-  end
-
-  private
-  def self.timeout
-    Rails.application.config.x.benchmarker.timeout || DEFAULT_TIMEOUT
+  def enqueued?
+    running? || waiting?
   end
 end
