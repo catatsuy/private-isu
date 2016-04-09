89ef9d862b10c06c8d21100c7ba5b1e0bc30d695
catatsuy <catatsuy@catatsuy.org>
2016-04-09 12:48:42 +0900

    clickしたらメッセージが全部見えるように

--- portal/app/assets/javascripts/application.js
+++ portal/app/assets/javascripts/application.js
@@ -15,3 +15,12 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+$(function() {
+  $('.expand-message').on('click', function() {
+    $this = $(this);
+    var msg = $this.attr('data-message');
+    $this.removeClass('clickable');
+    $this.text(msg);
+    $this.off();
+  });
+});
