c605d08df84a443c6ce14a2819edfa1fa1d6f5af
Atsushi Takayama <taka.atsushi@gmail.com>
2016-04-09 15:49:50 +0900

    time_wait?を復活

--- portal/app/models/job.rb
+++ portal/app/models/job.rb
@@ -1,4 +1,6 @@
 class Job < ActiveRecord::Base
+  DEFAULT_TIMEOUT = 60.seconds
+
   belongs_to :team
 
   def running?
@@ -7,5 +9,14 @@ class Job < ActiveRecord::Base
 
   def waiting?
     status == 'Waiting'
+  end
+
+  def self.time_wait?(team:)
+    where(team: team).where('created_at > ?', Time.current - timeout).count > 0
+  end
+
+  private
+  def self.timeout
+    Rails.application.config.x.benchmarker.timeout || DEFAULT_TIMEOUT
   end
 end
