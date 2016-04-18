af3a71060a79e747a1c95b37a7aa11d24be2a1cc
Keiji, Yoshimi <walf443@gmail.com>
2016-04-19 00:34:47 +0900

    getSessionUserを実装した

--- webapp/node/app.js
+++ webapp/node/app.js
@@ -1,8 +1,38 @@
 var express = require('express');
+var session = require('express-session');
 var ejs = require('ejs');
+var mysql = require('promise-mysql');
+var Promise = require('bluebird');
+
 var app = express();
 
+var db = mysql.createPool({
+  host: process.env.ISUCONP_DB_HOST || 'localhost',
+  port: process.env.ISUCONP_DB_PORT || 3306,
+  user: process.env.ISUCONP_DB_USER || 'root',
+  password: process.env.ISUCONP_DB_PASSWORD,
+  database: process.env.ISUCONP_DB_NAME || 'isuconp',
+  charset: 'utf8mb4'
+});
+
 app.engine('ejs', ejs.renderFile);
+app.use(session({
+  'resave': true,
+  'saveUninitialized': true,
+  'secret': process.env.ISUCONP_SESSION_SECRET || 'sendagaya'
+}));
+
+function getSessionUser(req) {
+  return new Promise(function(done, reject) {
+    if (!req.session.userId) {
+      done();
+      return;
+    }
+    db.query('SELECT * FROM `users` WHERE `id` = ?', [req.session.userId]).then(function(users) {
+      done(users[0]);
+    }).catch(reject);
+  });
+}
 
 app.get('/initialize', function(req, res) {
 });
@@ -23,7 +53,11 @@ app.get('/logout', function(req, res) {
 });
 
 app.get('/', function(req, res) {
-  res.render('index.ejs', { posts: [] });
+  getSessionUser(req).then(function(me) {
+    db.query('SELECT `id`, `user_id`, `body`, `created_at`, `mime` FROM `posts` ORDER BY `created_at` DESC').then(function(posts) {
+      res.render('index.ejs', { posts: posts, me: me });
+    });
+  });
 });
 
 app.get('/@(.+)/', function(req, res) {
