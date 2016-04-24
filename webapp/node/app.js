9ed6923d1fb07fe2113d70eb5c1172a2e63d824a
Keiji, Yoshimi <walf443@gmail.com>
2016-04-24 22:13:08 +0900

    ページングが動いていないのを修正

--- webapp/node/app.js
+++ webapp/node/app.js
@@ -367,7 +367,10 @@ app.get('/@:accountName/', (req, res) => {
 });
 
 app.get('/posts', (req, res) => {
-  let max_created_at = Date.parse(req.params.max_created_at) || new Date();
+  let max_created_at = new Date(req.query.max_created_at);
+  if (max_created_at.toString() === 'Invalid Date') {
+    max_created_at = new Date();
+  }
   db.query('SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `created_at` <= ? ORDER BY `created_at` DESC', max_created_at).then((posts) => {
     makePosts(posts.slice(0, POSTS_PER_PAGE * 2)).then((posts) => {
       getSessionUser(req).then((me) => {
