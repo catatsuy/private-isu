2cb7c842ed70ae3ffa7e7068bc5b3fd377d2a698
catatsuy <catatsuy@catatsuy.org>
2021-04-18 06:51:38 +0000

    update Gemfile for webapp ruby

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -2,10 +2,11 @@ require 'sinatra/base'
 require 'mysql2'
 require 'rack-flash'
 require 'shellwords'
+require 'rack/session/dalli'
 
 module Isuconp
   class App < Sinatra::Base
-    use Rack::Session::Memcache, autofix_keys: true, secret: ENV['ISUCONP_SESSION_SECRET'] || 'sendagaya'
+    use Rack::Session::Dalli, autofix_keys: true, secret: ENV['ISUCONP_SESSION_SECRET'] || 'sendagaya'
     use Rack::Flash
     set :public_folder, File.expand_path('../../public', __FILE__)
 
