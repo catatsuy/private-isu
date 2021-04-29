8482bcdb89c05e9fb0cb8a658c8e0d97ed274dab
catatsuy <catatsuy@catatsuy.org>
2021-04-29 13:29:41 +0900

    remove portal

--- portal/db/migrate/20160310142520_create_teams.rb
+++
@@ -1,9 +0,0 @@
-class CreateTeams < ActiveRecord::Migration
-  def change
-    create_table :teams do |t|
-      t.string :name
-      t.string :app_host
-      t.timestamps null: false
-    end
-  end
-end
