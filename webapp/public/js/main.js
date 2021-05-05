c241ec13c39ded58c25066d362a6af92a7e8a17d
catatsuy <catatsuy@catatsuy.org>
2021-05-05 12:48:09 +0900

    remove jquery for go

--- webapp/public/js/main.js
+++ webapp/public/js/main.js
@@ -1,25 +1,58 @@
 'use strict';
-$(function () {
-  $('#isu-post-more-btn').on('click', function () {
-    $('#isu-post-more').addClass('loading');
-    $.ajax({
-      type: 'GET',
-      url: '/posts',
-      data: {
-        max_created_at: $('.isu-post:last').attr('data-created-at')
-      },
-      dataType: 'html'
-    }).done(function (html) {
-      $(html).find('.isu-post').each(function() {
-        var id = $(this).attr('id');
-        if ($('#' + id).length === 0) {
-          $('.isu-posts').append($(this).clone());
+
+// cf: https://github.com/hustcc/timeago.js
+timeago.register('ja', (number, index) => {
+  return [
+    ['すこし前', 'すぐに'],
+    ['%s秒前', '%s秒以内'],
+    ['1分前', '1分以内'],
+    ['%s分前', '%s分以内'],
+    ['1時間前', '1時間以内'],
+    ['%s時間前', '%s時間以内'],
+    ['1日前', '1日以内'],
+    ['%s日前', '%s日以内'],
+    ['1週間前', '1週間以内'],
+    ['%s週間前', '%s週間以内'],
+    ['1ヶ月前', '1ヶ月以内'],
+    ['%sヶ月前', '%sヶ月以内'],
+    ['1年前', '1年以内'],
+    ['%s年前', '%s年以内'],
+  ][index];
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  timeago.render(document.querySelectorAll('time.timeago'), 'ja');
+
+  const btn = document.getElementById('isu-post-more-btn');
+  const postMore = document.getElementById('isu-post-more');
+
+  if (!btn) {
+    return;
+  }
+
+  btn.addEventListener('click', () => {
+    postMore.classList.add('loading');
+    const posts = document.querySelectorAll('.isu-post');
+    const lastEl = posts[posts.length-1];
+    const maxCreatedAt = lastEl.dataset.createdAt;
+    fetch(`/posts?max_created_at=${encodeURIComponent(maxCreatedAt)}`, {
+      method: 'GET',
+    }).then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.text();
+    }).then(text => {
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(text, "text/html");
+      doc.querySelectorAll('.isu-post').forEach((el) => {
+        const id = el.getAttribute('id');
+        if (!document.getElementById(id)) {
+          lastEl.parentElement.append(el);
         }
       });
-      $('time.timeago').timeago();
-      $('#isu-post-more').removeClass('loading');
+      timeago.render(document.querySelectorAll('time.timeago'), 'ja');
+      postMore.classList.remove('loading');
     });
   });
-
-  $('time.timeago').timeago();
 });
