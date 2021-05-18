3c5d59db2fd1ab0a9af62bb074f289fc31132658
ShinyaSakurai <31609483+ShinyaSakurai@users.noreply.github.com>
2021-05-18 14:15:45 +0900

    add mysql user

--- provisioning/image/files/home/isucon/env.sh
+++ provisioning/image/files/home/isucon/env.sh
@@ -1,2 +1,2 @@
 PATH=/usr/local/bin:/home/isucon/.local/ruby/bin:/home/isucon/.local/node/bin:/home/isucon/.local/python3/bin:/home/isucon/.local/perl/bin:/home/isucon/.local/php/bin:/home/isucon/.local/php/sbin:/home/isucon/.local/go/bin:/home/isucon/.local/scala/bin:/usr/bin/:/bin/:$PATH
-ISUCONP_DB_NAME=isuconp
+ISUCONP_DB_USER=isucon ISUCONP_DB_PASSWORD=isucon ISUCONP_DB_NAME=isuconp
