edbc5c0d5f3c3fbf4b4baffa1e9bfc0942c21b83
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-06-08 12:16:45 +0900

    features: get initialize

--- webapp/rust/src/main.rs
+++ webapp/rust/src/main.rs
@@ -1,14 +1,96 @@
-use std::{env, io};
+use std::{env, io, path::Path, time::Duration};
 
 use actix_cors::Cors;
 use actix_web::{
+    cookie::time::UtcOffset,
     error,
     http::{Method, StatusCode},
-    middleware, web, App, HttpRequest, HttpResponse, HttpServer,
+    middleware,
+    web::{self, Data},
+    App, HttpRequest, HttpResponse, HttpServer, Result,
 };
+use anyhow::Context;
+use chrono::{FixedOffset, Utc};
+use log::LevelFilter;
+use simplelog::{
+    ColorChoice, CombinedLogger, ConfigBuilder, SharedLogger, TermLogger, TerminalMode, WriteLogger,
+};
+use sqlx::{MySql, Pool};
+
+async fn db_initialize(pool: &Pool<MySql>) -> anyhow::Result<()> {
+    sqlx::query!("DELETE FROM users WHERE id > 1000")
+        .execute(pool)
+        .await
+        .context("Failed to db_initialize")?;
+    sqlx::query!("DELETE FROM posts WHERE id > 10000",)
+        .execute(pool)
+        .await
+        .context("Failed to db_initialize")?;
+    sqlx::query!("DELETE FROM comments WHERE id > 100000")
+        .execute(pool)
+        .await
+        .context("Failed to db_initialize")?;
+    sqlx::query!("UPDATE users SET del_flg = 0")
+        .execute(pool)
+        .await
+        .context("Failed to db_initialize")?;
+    sqlx::query!("UPDATE users SET del_flg = 1 WHERE id % 50 = 0")
+        .execute(pool)
+        .await
+        .context("Failed to db_initialize")?;
+
+    Ok(())
+}
+
+async fn get_initialize(pool: Data<Pool<MySql>>) -> Result<HttpResponse> {
+    if let Err(e) = db_initialize(&pool).await {
+        log::error!("{:?}", &e);
+    }
+    Ok(HttpResponse::Ok().finish())
+}
+
+fn init_logger<P: AsRef<Path>>(log_path: Option<P>) {
+    const JST_UTCOFFSET_SECS: i32 = 9 * 3600;
+
+    let jst_now = {
+        let jst = Utc::now();
+        jst.with_timezone(&FixedOffset::east(JST_UTCOFFSET_SECS))
+    };
+
+    let offset = UtcOffset::from_whole_seconds(JST_UTCOFFSET_SECS).unwrap();
+
+    let mut config = ConfigBuilder::new();
+    config.set_time_offset(offset);
+
+    let mut logger: Vec<Box<dyn SharedLogger>> = vec![
+        #[cfg(not(feature = "termcolor"))]
+        TermLogger::new(
+            if cfg!(debug_assertions) {
+                LevelFilter::Debug
+            } else {
+                LevelFilter::Info
+            },
+            config.build(),
+            TerminalMode::Mixed,
+            ColorChoice::Always,
+        ),
+    ];
+    if let Some(log_path) = log_path {
+        let log_path = log_path.as_ref();
+        std::fs::create_dir_all(&log_path).unwrap();
+        logger.push(WriteLogger::new(
+            LevelFilter::Info,
+            config.build(),
+            std::fs::File::create(log_path.join(format!("{}.log", jst_now))).unwrap(),
+        ));
+    }
+    CombinedLogger::init(logger).unwrap()
+}
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
+    init_logger::<&str>(None);
+
     let host = env::var("ISUCONP_DB_HOST").unwrap_or("localhost".to_string());
     let port: u32 = env::var("ISUCONP_DB_PORT")
         .unwrap_or("3306".to_string())
@@ -24,6 +106,15 @@ async fn main() -> io::Result<()> {
         &user, &password, &host, &port, &dbname
     );
 
+    let num_cpus = num_cpus::get();
+
+    let db = sqlx::mysql::MySqlPoolOptions::new()
+        .max_connections(num_cpus as u32)
+        .connect_timeout(Duration::from_secs(1))
+        .connect(&dsn)
+        .await
+        .unwrap();
+
     HttpServer::new(move || {
         App::new()
             .wrap(middleware::Logger::default())
@@ -32,6 +123,8 @@ async fn main() -> io::Result<()> {
             } else {
                 Cors::default().supports_credentials()
             })
+            .app_data(Data::new(db.clone()))
+            .service(web::resource("/initialize").route(web::get().to(get_initialize)))
             .service(
                 web::resource("/test").to(|req: HttpRequest| match *req.method() {
                     Method::GET => HttpResponse::Ok(),
