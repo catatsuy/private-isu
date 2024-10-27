6d0b3905e491278e30cff07f9998a6204c97f2ce
sanosuke39 <sanosuke3993@gmail.com>
2024-10-27 15:14:09 +0900

    INEDX貼ってみた。

--- webapp/sql/2_migration.sql
+++ webapp/sql/2_migration.sql
@@ -1,1 +1,5 @@
-CREATE INDEX idx_user_account_name ON users (account_name);
+CREATE INDEX idx_users_account_name ON users (account_name);
+CREATE INDEX idx_posts_created_at ON posts (created_at);
+CREATE INDEX idx_posts_user_id ON posts (user_id);
+CREATE INDEX idx_comments_user_id ON comments (user_id);
+CREATE INDEX idx_comments_post_id ON comments (post_id);
