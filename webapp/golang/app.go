4f2f48c9d969acca013a018996422e661c98bf7d
eakudompong.chanoknan <ceguantz96@outlook.com>
2025-06-11 15:37:00 +0900

    fix diff

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -386,7 +386,7 @@ func getIndex(w http.ResponseWriter, r *http.Request) {
 
 	results := []Post{}
 
-	err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `created_at` DESC")
+	err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `created_at` DESC LIMIT 20")
 	if err != nil {
 		log.Print(err)
 		return
